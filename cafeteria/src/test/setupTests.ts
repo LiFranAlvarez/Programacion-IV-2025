33a8bfd3ba0f73042d8f4baf19c4a8371d5f4160
MateoBelatti <mateobelatti7@gmail.com>
2025-10-29 12:11:07 -0300

    modificacion para que ande setupTest

--- cafeteria/src/test/setupTests.ts
+++ cafeteria/src/test/setupTests.ts
@@ -1,5 +1,5 @@
     import '@testing-library/jest-dom';
     import { server } from './mocks/server';
-    beforeAll(() => server.listen());
+    beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
     afterEach(() => server.resetHandlers());
     afterAll(() => server.close()); 
